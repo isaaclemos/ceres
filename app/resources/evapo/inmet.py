6be794e2326865f16d37acaadb314c3120ed6d20
isaaclemos <isaacemossjn@gmail.com>
2022-11-29 00:47:05 -0300

    ss

--- app/resources/evapo/inmet.py
+++ app/resources/evapo/inmet.py
@@ -1,13 +1,13 @@
-from inmetpy.inmet_stations import InmetStation
+from inmetpy.stations import InmetStation
 
 
 inmet = InmetStation()
 # Dia da a q u i s i c a o da imagem
-data_ini = "2022-6-30"
-data_fim = "2022-6-30"
+data_ini = "2017-10-11"
+data_fim = "2017-10-11"
 ordem = "hour"
 # Parametro inserido ao cadastrar uma estacao de captura dos dados
-cod_estacoes = [" A840 "]
+cod_estacoes = ["A840"]
 # E gerado um dataframe com todos os dados das estacoes
 dados = inmet.get_data_station(data_ini, data_fim, ordem, cod_estacoes)
-1
+print(dados['TEMP'])
---
+++ app/resources/evapo/main.py
@@ -0,0 +1,7 @@
+import os
+
+
+os.chdir('images')
+
+files = os.listdir()
+
